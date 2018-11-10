a90799a9191fcdb047baf06ffec508417673077d
David Rendleman <david.rendleman@gmail.com>
2018-11-09 21:21:15 -0800

    chunya250 initial commit

---
+++ random_agent.py
@@ -0,0 +1,21 @@
+"""A random agent which records minimap features."""
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import numpy
+
+from pysc2.agents import base_agent
+from pysc2.lib import actions
+
+
+class RandomAgent(RecordingAgent):
+  """A random agent for starcraft."""
+
+  def step(self, obs):
+    super(RandomAgent, self).step(obs)
+    function_id = numpy.random.choice(obs.observation.available_actions)
+    args = [[numpy.random.randint(0, size) for size in arg.sizes]
+            for arg in self.action_spec.functions[function_id].args]
+    return actions.FunctionCall(function_id, args)
